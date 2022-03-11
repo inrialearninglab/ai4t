670e1f10c499e51f74041d342495ddafadcad7c8
Benoit Rospars <brospars@users.noreply.github.com>
2022-03-11 10:40:44 +0100

    Add piwik

--- docs/assets/extra.js
+++ docs/assets/extra.js
@@ -11,3 +11,15 @@ window.MathJax = {
   }
 };
 
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://piwik.inria.fr/";
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  _paq.push(['setSiteId', '126']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+
