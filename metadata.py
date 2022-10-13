e4a71694aa1d25b34f10f33a13a668b1b3f770a4
Benoit Rospars <benoit.rospars@inria.fr>
2022-10-13 12:06:06 +0200

    Update script to generate metadata

--- metadata.py
+++ metadata.py
@@ -1,36 +1,64 @@
 import os
+import markdown
+import re
 from datetime import datetime
 
 xmlTemplate = """<?xml version="1.0"?>
 <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
     <dc:identifier>{uid}</dc:identifier>
-    <dc:type>{media}</dc:type>
-    <dc:title>Is AI relevant to education ?</dc:title>
-    <dc:contributor>AI4T</dc:contributor>
-    <dc:subject>{keyword}</dc:subject>
+    <dc:type>{type}</dc:type>
+    <dc:title>{title}</dc:title>
+    <dc:subject>Artificial Intelligence for and by Teachers</dc:subject>
+    <dc:description>{description}</dc:description>
     <dc:publisher>AI4T</dc:publisher>
+    <dc:contributor>AI4T</dc:contributor>{contributor}
     <dc:date>{date}</dc:date>
-    <dc:publisher>AI4T</dc:publisher>
     <dc:rights>cc by 4.0</dc:rights>
     <dc:language>{lang}</dc:language>
 </metadata>
 """
 
-metadata = dict(
-    uid="EU.AI4T.O1.M1.1.1.0",
-    media="Text",
-    keyword="keyword",
-    date=datetime.now().strftime("%Y-%m-%d"),
-    lang="en"
-)
-
-print(xmlTemplate.format(**metadata))
-
-basepath = u"./docs/"
+basepath = u"./docs/O1-Mooc/"
 for path, dirs, files in os.walk(basepath):
     for file in files:
-        filename, file_extension = os.path.splitext(file)
-        if file_extension == '.md' and filename != "index":
-            print(os.path.join(path, filename, '.xml'))
-            with open(os.path.join(path, filename+'.xml'), 'w+') as f:
-                f.write(xmlTemplate.format(**metadata))
+        file_no_ext, file_extension = os.path.splitext(file)
+        file_no_lang, lang_extension = os.path.splitext(file_no_ext)
+        if file_extension == '.md' and file_no_ext != "index":
+            with open(os.path.join(path, file), 'r') as i:
+                md = markdown.Markdown(extensions=['meta'])
+                md.convert(i.read())
+                # print(os.path.join(path, file_no_ext))
+                # print(md.Meta)
+                match_uid = re.search(
+                    r"docs\/(..[^-]?)[^\/]*\/.*\/(\d)-(\d)-(\d)?([av])?.*",
+                    os.path.join(path, file_no_ext)
+                )
+                if match_uid:
+                    parsed_uid = dict(
+                        part=match_uid.group(1),
+                        module=match_uid.group(2),
+                        sequence=match_uid.group(3),
+                        unit=match_uid.group(4) if match_uid.group(4) else '0',
+                        type=match_uid.group(5) if match_uid.group(5) else 't'
+                    )
+                    uid = "EU.AI4T.{part}.M{module}.{sequence}.{unit}{type}".format(**parsed_uid)
+                else:
+                    print(os.path.join(path, file_no_ext))
+                    uid = "EU.AI4T.@todo"
+                contributor = ""
+                if 'contributor' in md.Meta:
+                    contributor += '\n    <dc:contributor>'
+                    contributor += '</dc:contributor>\n    <dc:contributor>'.join(md.Meta['contributor'])
+                    contributor += '</dc:contributor>'
+                metadata = dict(
+                    uid=uid,
+                    type=md.Meta.get('type', ['Text'])[0],
+                    title=md.Meta.get('title', [''])[0],
+                    description=md.Meta.get('description', [''])[0],
+                    contributor=contributor,
+                    date=datetime.now().strftime("%Y-%m-%d"),
+                    lang=lang_extension[1:]
+                )
+                # print(os.path.join(path, file_no_ext, '.xml'))
+                with open(os.path.join(path, file_no_ext+'.xml'), 'w+') as o:
+                    o.write(xmlTemplate.format(**metadata))
